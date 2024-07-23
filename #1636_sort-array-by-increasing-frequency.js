dbf571000e8b770e301c136a8a6fc8a15f74bb03
Kate Iliu <beviatori@gmail.com>
2024-07-23 09:54:10 -0600

    Rename all files

---
+++ #1636_sort-array-by-increasing-frequency.js
@@ -0,0 +1,12 @@
+//##1636
+// Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+// Return the sorted array.
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var frequencySort = function(nums) {
+    
+};
+
+const nums = [1,1,2,2,2,3]
